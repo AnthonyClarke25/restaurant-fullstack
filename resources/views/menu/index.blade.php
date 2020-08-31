49628046cb7a6185ecaa34c3b933e4fe19ef95df
Anthony Clarke <luda59@hotmail.com>
2020-08-31 14:03:39 +0100

    Made template layouts

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -4,8 +4,7 @@
 
 @section('content')
  <div class="welcome-jumbo">
-      <div class="status">OLD</div>
-      <h1>Billy Burger</h1>
-      <img src="/img/hamburger-and-fries-png-4.png" class="burger-fries">
+      <h1>MENU PAGE</h1>
+    
 </div>
 @endsection
